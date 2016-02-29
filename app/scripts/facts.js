eb20fd2b6c218d32c72728da44bfd50ef8fb45a1
Peter Hartree <web@peterhartree.co.uk>
2016-02-29 18:07:16 +0000

    Add comments to fact provider
    Signed-off-by: Peter Hartree <web@peterhartree.co.uk>

--- app/scripts/facts.js
+++ app/scripts/facts.js
@@ -1,27 +1,39 @@
 var ReducetarianFacts = (function() {
 
   var facts = {};
-  facts.globalAverageDailyConsumption = 0.1;
+  facts.globalAverageDailyConsumption = 0.1; // lbs
   facts.portionSizes = [];
 
+  // If a user self-reports one meaty meal per day we assume
+  // they're consuming 0.23 lbs of meat per day.
+  //
+  // This puts them in 82nd percentile of global daily consumption.
   facts.portionSizes[1] = {
     'lbs' : 0.23,
     'globalPercentile' : 82
   };
 
+  // If a user self-reports two meaty meals per day we assume
+  // they're consuming 0.34 lbs of meat per day.
+  //
+  // This puts them in 96th percentile of global daily consumption.
   facts.portionSizes[2] = {
     'lbs' : 0.34,
     'globalPercentile' : 96
   };
 
+  // If a user self-reports three meaty meals per day we assume
+  // they're consuming 0.45 lbs of meat per day.
+  //
+  // This puts them in 99.5th percentile of global daily consumption.
   facts.portionSizes[3] = {
     'lbs' : 0.45,
     'globalPercentile' : 99.5
   };
 
-  facts.cO2PerLbOfConsumption = 14.7;
-  facts.waterPerLbOfConsumption = 5.3;
-  facts.animalLivesPerLbOfConsumption = 0.16;
+  facts.cO2PerLbOfConsumption = 14.7; // lbs
+  facts.waterPerLbOfConsumption = 5.3; // gallons
+  facts.animalLivesPerLbOfConsumption = 0.16; // lives
 
   return {
       get: function() {
